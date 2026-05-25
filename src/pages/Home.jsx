24449e5c088fd73ad6d6eddd08b914d97b281ec0
pashupat-git <mahimac346@gmail.com>
2026-05-25 15:53:27 +0545

    font changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
       <h2 className={Styles.fadeItem} style={{ animationDelay: '0.6s' }}>
         Welcome to Mahima Living
       </h2>
-      <button onClick={() => navigate('/gallery')} className={`${Styles.fadeItem} bg-black/40 text-sm  border-2 rounded-2xl px-3 py-2 cursor-pointer 
+      <button onClick={() => navigate('/gallery')} className={`${Styles.fadeItem} bg-black/40 text-sm  border-2 rounded-2xl px-3 py-2 cursor-grab 
       tracking-widest transition-all duration-300 ease-in-out hover:bg-white hover:text-black`}>
         SHOP NOW 
       </button>
@@ -30,6 +30,7 @@ function Home() {
     </div>
 
     <div className={Styles.rightCol}>
+      <h1 className="text-5xl text-amber-800">About Modern Homes</h1>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit.
         Minus commodi quibusdam blanditiis quia cupiditate laborum
@@ -37,11 +38,10 @@ function Home() {
         temporibus libero ducimus consequatur voluptatum expedita
         sunt obcaecati.
       </p>
-      <div className={Styles.imageRow}>
-      <img src="/images/sofa1.jpg" alt="sofa1" className={Styles.smallImg} />
-      <img src="/images/sofa1.jpg" alt="sofa1" className={Styles.smallImg} />
-      <img src="/images/sofa1.jpg" alt="sofa1" className={Styles.smallImg} />
-    </div>
+
+     <button onClick={()=> navigate('/contact')}
+     className="border-2 rounded-2xl bg-blue-600 text-amber-50 px-2 py-2 w-50 cursor-grab"
+     >Get in Touch</button>
   </div>
 
   </div>
